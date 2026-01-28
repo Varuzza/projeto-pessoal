cc6ee95ee31e2ffd6c4521efac58b85ea0f024ff
Leonardo <leonardovaruzza@hotmail.com>
2026-01-28 06:34:41 +0100

    mensagem de comomit

---
+++ app.js
@@ -0,0 +1,22 @@
+let titulo = document.querySelector('h1')
+titulo.innerHTML = 'Hora do Desafio!'
+
+function consoleClicado() {
+    console.log('O Botão foi clicado!')
+}
+
+function botaoAlerta() {
+    alert('Eu amo JS!')
+}
+
+function promptCidade() {
+    let nomeCidade = prompt('Qual é o nome da cidade onde mora?')
+    alert(`Estive em ${nomeCidade} e lembrei de você!`)
+}
+
+function somaInteiro() {
+    let numero1 = prompt('Digite um número inteiro');
+    let numero2 = prompt('Digite outro número inteiro');
+    let soma = Number(numero1) + Number(numero2)
+    alert(`A soma de ${numero1} e ${numero2} é igual a ${soma}`)
+}
