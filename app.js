707efe9699fe886235b16ea88b1681e4e07c9df1
Leonardo <leonardovaruzza@hotmail.com>
2026-01-31 15:15:27 +0100

    Deixando a variável nome Global

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'Hora do Desafio!'
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio!';
+let nome = prompt('Digite seu nome completo');
 
 function consoleClicado() {
-    console.log('Olá, tudo bem? O Botão foi clicado!')
+    console.log('Olá, tudo bem? O Botão foi clicado!');
 }
 
 function botaoAlerta() {
-    alert('Eu amo JS!')
+    alert('Eu amo JS!');
 }
 
 function promptCidade() {
-    let nome = prompt('Digite seu nome completo')
-    let nomeCidade = prompt('Qual é o nome da cidade onde mora?')
-    alert(`Olá ${nome}! Estive em ${nomeCidade} e lembrei de você!`)
+    let nomeCidade = prompt('Qual é o nome da cidade onde mora?');
+    alert(`Olá ${nome}! Estive em ${nomeCidade} e lembrei de você!`);
 }
 
 function somaInteiro() {
     let numero1 = prompt('Digite um número inteiro');
     let numero2 = prompt('Digite outro número inteiro');
-    let soma = Number(numero1) + Number(numero2)
-    alert(`A soma de ${numero1} e ${numero2} é igual a ${soma}`)
+    let soma = Number(numero1) + Number(numero2);
+    alert(`A soma de ${numero1} e ${numero2} é igual a ${soma}`);
 }
